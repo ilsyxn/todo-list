7d7d756453c97cac75dbf46adf2018899834b4af
Ileyan <3003916@stud.hs-mannheim.de>
2024-08-29 19:43:56 +0200

    Edit Button hinzugefügt, um mehrere Elemente auszuwählen

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   let knopf = document.getElementById("add-btn");
   let liste = document.getElementById("liste");
   let eingabe = document.getElementById("eingabe");
+  let editBtn = document.getElementById("edit-btn");
+  let selected = [];
+  let edit = false;
 
   function addTask() {
     if (eingabe.value != "") {
       let li = document.createElement("li");
       li.textContent = eingabe.value;
       li.appendChild(getDeleteButton());
+      li.addEventListener("click", function () {
+        select(li);
+      });
       liste.appendChild(li);
       eingabe.value = "";
     }
@@ -15,18 +21,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function getDeleteButton() {
     let del = document.createElement("button");
-    del.textContent = "DELETE";
+    let icon = document.createElement("img");
+    del.setAttribute("id", "delete-btn");
+    icon.setAttribute("src", "img/trash.png");
+    icon.setAttribute("width", "22");
+    del.appendChild(icon);
     del.addEventListener("click", function () {
       liste.removeChild(del.parentNode);
     });
     return del;
   }
 
-  knopf.addEventListener("click", addTask);
+  function select(elem) {
+    if (edit) {
+      if (selected.includes(elem)) {
+        selected = selected.filter((item) => item !== elem);
+        elem.style.backgroundColor = "";
+      } else {
+        selected.push(elem);
+        elem.style.backgroundColor = "#ddd";
+      }
+    }
+  }
+
+  function toggleButton() {
+    knopf.textContent = edit ? "DELETE" : "ADD";
+  }
+
+  function deleteSelected() {
+    selected.forEach((elem) => {
+      liste.removeChild(elem);
+    });
+    selected = [];
+  }
+
+  knopf.addEventListener("click", function () {
+    if (!edit) {
+      addTask();
+    } else {
+      deleteSelected();
+    }
+  });
+  editBtn.addEventListener("click", function () {
+    edit = !edit;
+    toggleButton();
+  });
 
   document.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
       addTask();
+    }
+  });
+
+  eingabe.addEventListener("click", function () {
+    if (edit) {
+      edit = false;
+      toggleButton();
     }
   });
 });
